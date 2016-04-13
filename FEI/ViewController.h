9e834edd0f4535fb273087d71c4a94a01873477b
Sanket <sanket@sankets-macbook-pro.local>
2016-04-13 22:21:16 +0530

    Initial Commit

---
+++ FEI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FEI
+//
+//  Created by Sanket on 13/04/16.
+//  Copyright © 2016 Zresthine Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FEI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FEI
+//
+//  Created by Sanket on 13/04/16.
+//  Copyright © 2016 Zresthine Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
